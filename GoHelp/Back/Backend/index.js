920497eb36aa87faf78f4bdc16ccae1e0519e0c7
RafaSC11 <rafael.s2003@gmail.com>
2024-05-23 14:59:39 +0100

    .

--- GoHelp/Back/Backend/index.js
+++ GoHelp/Back/Backend/index.js
@@ -22,7 +22,7 @@ function loadInitiatives(map) {
   const initiatives = JSON.parse(localStorage.getItem("initiatives")) || [];
   const tbody = document.getElementById("initiatives-tbody");
   const initiativesIniciadas = initiatives.filter(
-    (initiative) => initiative.status === "aprovada" //para testes enquanto nao esta a funcionar, será a decorrer
+    (initiative) => initiative.status === "A decorrer" //para testes enquanto nao esta a funcionar, será a decorrer
   )
   tbody.innerHTML = '';
 
